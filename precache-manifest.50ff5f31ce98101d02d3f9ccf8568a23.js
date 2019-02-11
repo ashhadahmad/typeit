16013ed75f01455975c69860e0eace7b696837a5
Ashhad Ahmad <ashhadahmed0@gmail.com>
2019-02-11 15:29:40 +0530

    Updates

---
+++ precache-manifest.50ff5f31ce98101d02d3f9ccf8568a23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73d3efed35ab8a542098",
+    "url": "/static/js/main.73d3efed.chunk.js"
+  },
+  {
+    "revision": "142201303e1a57327148",
+    "url": "/static/js/1.14220130.chunk.js"
+  },
+  {
+    "revision": "73d3efed35ab8a542098",
+    "url": "/static/css/main.8d882f0d.chunk.css"
+  },
+  {
+    "revision": "e3f1cd516edc8013b92c8ada09d5651a",
+    "url": "/index.html"
+  }
+];
